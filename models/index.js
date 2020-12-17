f50ea1b273726df03b73f010b7987badb0d1bff2
jtreeves <jackson.t.reeves@gmail.com>
2020-12-17 13:01:48 -0500

    [bug] Fix database connection by requiring dotenv

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 // Set up MongoDB connection
@@ -7,8 +8,11 @@ mongoose.connect(process.env.MONGO_URI, {
     useFindAndModify: true
 })
 
+console.log(process.env.MONGO_URI)
+
 // Set database with mongoose connection object
 const db = mongoose.connection
+console.log(db)
 
 // Set up an event listener that will fire once the connection opens for the database
 // Log to the terminal what host and port we are on
@@ -20,4 +24,4 @@ db.on('error', () => {
     console.log(`Database error\n ${error}`)
 })
 
-module.exports.User = require('./User')
+// module.exports.User = require('./User')
