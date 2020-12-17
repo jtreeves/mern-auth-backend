e32a0f24268c038aeaf45600a67ae0427243256e
jtreeves <jackson.t.reeves@gmail.com>
2020-12-17 16:02:10 -0500

    [users] Create register route

--- controllers/users.js
+++ controllers/users.js
@@ -9,9 +9,46 @@ const passport = require('passport')
 // Models
 const User = require('../models/User')
 
-// GET route for users/test
+// GET route for users/test (Public)
 router.get('/test', (req, res) => {
     res.json({msg: 'User endpoint OK!'})
+})
+
+// POST route for users/register (Public)
+router.post('/register', (req, res) => {
+    // Find user by email
+    User
+        .findOne({email: req.body.email})
+        .then(user => {
+            // If email already exists, send 400 response
+            if (user) {
+                return res.status(400).json({msg: 'Email already exists'})
+            } else {
+                // Create a new user
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+                // Salt and hash password, then save new user
+                bcrypt.genSalt(10, (error, salt) => {
+                    if (error) throw Error
+                    bcrypt.hash(newUser.password, salt, (error, hash) => {
+                        if (error) throw Error
+                        // Change password to the hash version
+                        newUser.password = hash
+                        newUser
+                            .save()
+                            .then(createdUser => {
+                                return res.json(createdUser)
+                            })
+                            .catch(error => {
+                                console.log(error)
+                            })
+                    })
+                })
+            }
+        })
 })
 
 module.exports = router
