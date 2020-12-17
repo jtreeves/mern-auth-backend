c601a55c440ce54d1bc3f72a1b6369c444a462db
jtreeves <jackson.t.reeves@gmail.com>
2020-12-17 12:45:20 -0500

    [models] Add error message with a line break

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+// Set up MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopography: true,
+    useFindAndModify: true
+})
+
+// Set database with mongoose connection object
+const db = mongoose.connection
+
+// Set up an event listener that will fire once the connection opens for the database
+// Log to the terminal what host and port we are on
+db.once('open', () => {
+    console.log(`Connected to MongoDB at ${db.host} on ${db.port}`)
+})
+
+db.on('error', () => {
+    console.log(`Database error\n ${error}`)
+})
+
+module.exports.User = require('./User')
