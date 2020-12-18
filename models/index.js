9dbf8918c13aa89d767fe958b7a86effc12a24c6
jtreeves <jackson.t.reeves@gmail.com>
2020-12-18 18:47:14 -0500

    [models/index] Change 'Topography' to 'Topology'

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 // Set up MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopography: true,
+    useUnifiedTopology: true,
     useFindAndModify: true
 })
 
